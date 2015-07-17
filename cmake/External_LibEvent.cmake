09807b9b3760d59afe916d9c5b4ac6e2a3c36193
Nate Rosenblum <flander@gmail.com>
2015-07-17 15:02:59 -0700

    Find windows config header

--- cmake/External_LibEvent.cmake
+++ cmake/External_LibEvent.cmake
@@ -2,4 +2,7 @@ find_package(LibEvent)
 
 if(LIBEVENT_FOUND)
     include_directories(${LibEvent_INCLUDE_DIR})
+    if(WIN32)
+        include_directories(${LibEvent_WIN_INCLUDE_DIR})
+    endif(WIN32)
 endif()
